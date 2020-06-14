25ca3491636db0aded54263c94a60ce08544af53
rygrass <60543137+rygrass@users.noreply.github.com>
2020-06-14 23:14:03 +0800

    Added Files
    Added Files needed

---
+++ zserv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+function explodemaps() {
+	for map in $(echo $1 | tr ',' '\n'); do echo "addmap $map"; done
+}
+
+[[ -n $HOSTNAME ]] && HOSTNAME="set hostname \"$HOSTNAME\""
+[[ -n $MOTD ]] && MOTD="set motd \"$HOSTNAME\""
+[[ -n $RCON_PASSWORD ]] && RCON_PASSWORD="set rcon_password \"$RCON_PASSWORD\""
+[[ -n $PASSWORD ]] && PASSWORD="set password \"$PASSWORD\""
+[[ $LAN -ge 1 ]] && MASTER_ADVERTISE="set master_advertise 2" || MASTER_ADVERTISE="set master_advertise 1"
+[[ -n $MAXPLAYERS ]] && MAXPLAYERS="set maxclients $MAXPLAYERS
+set maxclients $SV_MAXPLAYERS"
+
+# Default maps
+[[ -z $MAPS ]] && MAPS="map01,map04,map07"
+
+cat <<EOF > overrides.cfg
+$HOSTNAME
+$MOTD
+$RCON_PASSWORD
+$MASTER_ADVERTISE
+$PASSWORD
+$MAXPLAYERS
+// Splits up comma separated map list
+$(explodemaps $MAPS)
+// Random CVARs from the environment
+$(env | grep '^CVAR_' | sed "s/^CVAR_/set /" | tr '=' ' ')
+EOF
+
+
+exec ./zserv -waddir wads -cfg zserv.cfg -port 10701 -ip 192.168.0.146
